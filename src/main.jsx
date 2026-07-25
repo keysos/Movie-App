97a1ce4fc33a1a3d4c53402e008309e9200899cc
Keysos <gustavoxabreux@gmail.com>
2026-07-25 19:42:17 -0300

    feat: add pwa to support web apps

--- src/main.jsx
+++ src/main.jsx
@@ -6,6 +6,11 @@ import App from './App.jsx'
 import { FavoritesProvider } from './context/FavoritesContext.jsx'
 import { WatchlistProvider } from './context/WatchlistContext.jsx'
 import { ThemeToggleProvider } from './context/ThemeToggleContext.jsx'
+import { registerSW } from 'virtual:pwa-register'
+
+registerSW({
+  immediate: true,
+})
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
